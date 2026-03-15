8eb1496280050342af3fba53f2ed43a1c029056f
Stanislav Snagin <s_lstoo@bk.ru>
2026-03-16 02:21:24 +0300

    deployment to github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || '/'
+  },
+
+  nitro: {
+    prerender: {
+      failOnError: false
+    }
+  }
 })
