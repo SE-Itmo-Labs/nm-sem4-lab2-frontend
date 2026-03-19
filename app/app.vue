ec3e3a010b4c1c2077c9e17082051d71d4cfaddb
Stanislav Snagin <s_lstoo@bk.ru>
2026-03-19 15:15:34 +0300

    some trash

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    Test
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,5 +6,13 @@ export default defineNuxtConfig({
   app: {
     baseURL: '/nm-sem4-lab2-frontend/', // baseURL: '/<repository>/'
     buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+    head: {
+      script: [
+        {
+          src: 'https://www.desmos.com/api/v1.9/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6',
+          async: true
+        }
+      ]
+    }
   }
 })
